31cdb1d360a278e16b6282d6f2dd0920ed0b1ebb
jyotipatelsjsu <78842948+jyotipatelsjsu@users.noreply.github.com>
2021-03-13 00:28:20 -0800

    Adding preprocessed file.

--- src/controller/openaq.py
+++ src/controller/openaq.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -9,6 +9,13 @@ app = Flask(__name__)
 def hello():
     return 'Hello, This is team Avenger OPENAQ project for class DATA228!'
 
-@app.route('/openaq/get_data_by_date')
+
+@app.route('/openaq/get_data_by_date', methods=['GET', 'POST'])
 def get_openaq_data_by_date():
+    return
+
+
+@app.route('/openaq/topCitiesByParam', methods=['GET', 'POST'])
+def get_top_cities_by_param():
+    data = request.get_json()
     return
