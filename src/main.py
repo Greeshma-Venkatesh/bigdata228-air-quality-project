e9cf2fee4875a181bfaddf903804ad5054a00827
jyotipatelsjsu <78842948+jyotipatelsjsu@users.noreply.github.com>
2021-05-07 19:18:23 -0700

    Added CORS for UI

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS, cross_origin
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 from controller import openaq
